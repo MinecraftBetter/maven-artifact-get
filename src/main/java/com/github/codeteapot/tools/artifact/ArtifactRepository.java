c76284092fc78c69b3e5a54f5629eb920bff9904
EvanG <evan_g@orange.fr>
2023-02-18 15:57:35 +0100

    Read .pom files without namespace

--- src/main/java/com/github/codeteapot/tools/artifact/ArtifactRepository.java
+++ src/main/java/com/github/codeteapot/tools/artifact/ArtifactRepository.java
@@ -1,10 +1,15 @@
 package com.github.codeteapot.tools.artifact;
 
-import static java.util.Objects.requireNonNull;
-import static java.util.Optional.ofNullable;
-import static java.util.stream.Collectors.toMap;
-import static javax.xml.bind.JAXBContext.newInstance;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.sax.SAXSource;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
@@ -14,88 +19,105 @@ import java.util.AbstractMap.SimpleEntry;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Stream;
-import javax.xml.bind.JAXBException;
+
+import static java.util.Objects.requireNonNull;
+import static java.util.Optional.ofNullable;
+import static java.util.stream.Collectors.toMap;
 
 /**
  * Artifact repository.
  */
 public class ArtifactRepository {
 
-  private static final Map<String, String> EXTENSION_MAP = Stream.of(
-      new SimpleEntry<>("jar", "jar"),
-      new SimpleEntry<>("ejb", "jar"),
-      new SimpleEntry<>("ejb-client", "jar"),
-      new SimpleEntry<>("war", "war"),
-      new SimpleEntry<>("ear", "ear"),
-      new SimpleEntry<>("pom", "pom")).collect(toMap(Entry::getKey, Entry::getValue));
+    private static final Map<String, String> EXTENSION_MAP = Stream.of(
+            new SimpleEntry<>("jar", "jar"),
+            new SimpleEntry<>("ejb", "jar"),
+            new SimpleEntry<>("ejb-client", "jar"),
+            new SimpleEntry<>("war", "war"),
+            new SimpleEntry<>("ear", "ear"),
+            new SimpleEntry<>("pom", "pom")).collect(toMap(Entry::getKey, Entry::getValue));
 
-  private static final String DEFAULT_EXTENSION = "jar";
+    private static final String DEFAULT_EXTENSION = "jar";
 
-  private final URL directory;
+    private final URL directory;
 
-  /**
-   * Repository at given directory URL.
-   *
-   * @param directory Directory URL.
-   */
-  public ArtifactRepository(URL directory) {
-    this.directory = requireNonNull(directory);
-  }
-
-  /**
-   * Get an artifact through this repository.
-   *
-   * @param coordinates Artifact coordinates.
-   *
-   * @return The artifact.
-   *
-   * @throws ArtifactRepositoryException When some repository error has been occurred.
-   * @throws IOException When an I/O error has been occurred.
-   */
-  public Artifact get(ArtifactCoordinates coordinates)
-      throws ArtifactRepositoryException, IOException {
-    try (InputStream input = file(coordinates.getPath("pom")).openStream()) {
-      XMLProject project = (XMLProject) newInstance(XMLProject.class)
-          .createUnmarshaller()
-          .unmarshal(input);
-      return new Artifact(
-          file(coordinates.getPath(project.getExtension(this::fromPackaging))),
-          project.getDependencies());
-    } catch (JAXBException | URISyntaxException | MalformedURLException e) {
-      throw new ArtifactRepositoryException(e);
-    } catch (UncheckedArtifactRepositoryException e) {
-      throw e.getCause();
+    /**
+     * Repository at given directory URL.
+     *
+     * @param directory Directory URL.
+     */
+    public ArtifactRepository(URL directory) {
+        this.directory = requireNonNull(directory);
     }
-  }
 
-  /**
-   * Hash based on {@code directory}.
-   */
-  @Override
-  public int hashCode() {
-    return directory.hashCode();
-  }
+    /**
+     * Get an artifact through this repository.
+     *
+     * @param coordinates Artifact coordinates.
+     * @return The artifact.
+     * @throws ArtifactRepositoryException When some repository error has been occurred.
+     * @throws IOException                 When an I/O error has been occurred.
+     */
+    public Artifact get(ArtifactCoordinates coordinates)
+            throws ArtifactRepositoryException, IOException {
+        try (InputStream input = file(coordinates.getPath("pom")).openStream()) {
+            //Prepare JAXB objects
+            JAXBContext jc = JAXBContext.newInstance(XMLProject.class);
+            Unmarshaller u = jc.createUnmarshaller();
 
-  /**
-   * Equality based on {@code directory}.
-   */
-  @Override
-  public boolean equals(Object obj) {
-    if (obj == this) {
-      return true;
+            //Create an XMLReader to use with our filter
+            XMLReader reader = SAXParserFactory.newInstance().newSAXParser().getXMLReader();
+
+            //Create the filter (to add namespace) and set the xmlReader as its parent.
+            NamespaceFilter inFilter = new NamespaceFilter("http://maven.apache.org/POM/4.0.0", true);
+            inFilter.setParent(reader);
+
+            //Prepare the input
+            InputSource is = new InputSource(input);
+
+            //Create a SAXSource specifying the filter
+            SAXSource source = new SAXSource(inFilter, is);
+
+            //Do unmarshalling
+            XMLProject project = (XMLProject) u.unmarshal(source);
+            return new Artifact(
+                    file(coordinates.getPath(project.getExtension(this::fromPackaging))),
+                    project.getDependencies());
+        } catch (JAXBException | URISyntaxException | MalformedURLException | ParserConfigurationException | SAXException e) {
+            throw new ArtifactRepositoryException(e);
+        } catch (UncheckedArtifactRepositoryException e) {
+            throw e.getCause();
+        }
     }
-    if (obj instanceof ArtifactRepository) {
-      ArtifactRepository repository = (ArtifactRepository) obj;
-      return directory.equals(repository.directory);
+
+    /**
+     * Hash based on {@code directory}.
+     */
+    @Override
+    public int hashCode() {
+        return directory.hashCode();
     }
-    return false;
-  }
 
-  private String fromPackaging(String packaging) {
-    return ofNullable(packaging).map(EXTENSION_MAP::get).orElse(DEFAULT_EXTENSION);
-  }
+    /**
+     * Equality based on {@code directory}.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj instanceof ArtifactRepository) {
+            ArtifactRepository repository = (ArtifactRepository) obj;
+            return directory.equals(repository.directory);
+        }
+        return false;
+    }
 
-  private URL file(String relativePath) throws URISyntaxException, MalformedURLException {
-    return new URL(directory, relativePath);
-  }
+    private String fromPackaging(String packaging) {
+        return ofNullable(packaging).map(EXTENSION_MAP::get).orElse(DEFAULT_EXTENSION);
+    }
+
+    private URL file(String relativePath) throws URISyntaxException, MalformedURLException {
+        return new URL(directory, relativePath);
+    }
 }
